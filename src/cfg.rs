fb70bef9a1727ceb1ab6955fd22e9db719604ac7
Tanner Sandlin <tanndlin@gmail.com>
2024-05-14 23:40:59 -0500

    Allow strings as variables

--- src/cfg.rs
+++ src/cfg.rs
@@ -2,13 +2,13 @@ use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
 struct Variable {
-    pub name: char,
-    productions: Vec<Vec<char>>,
+    pub name: String,
+    productions: Vec<Vec<String>>,
 }
 impl Variable {
-    fn remove_null_production(&mut self, nullable_name: char) {
+    fn remove_null_production(&mut self, nullable_name: &str) {
         // Case where there is only 1 production
-        let mut new_prods: Vec<Vec<char>> = vec![];
+        let mut new_prods: Vec<Vec<String>> = vec![];
 
         // For each current production
         self.productions.iter().for_each(|x| {
@@ -25,23 +25,23 @@ impl Variable {
         // Remove the production with just the null symbol
         self.productions = new_prods
             .iter()
-            .filter(|x| x.len() > 1 || (x.len() == 1 && x.first().unwrap() != &'#'))
+            .filter(|x| x.len() > 1 || (x.len() == 1 && x[0] != "#"))
             .map(|x| x.clone())
             .collect();
     }
 }
 
-fn permute_without(chars: Vec<char>, to_remove: &char) -> Vec<Vec<char>> {
+fn permute_without(strings: Vec<String>, to_remove: &str) -> Vec<Vec<String>> {
     let mut perms = vec![];
 
-    if !chars.iter().any(|c| c == to_remove) {
-        return vec![chars];
+    if !strings.iter().any(|c| c == to_remove) {
+        return vec![strings];
     }
 
-    chars.iter().enumerate().for_each(|(index, c)| {
+    strings.iter().enumerate().for_each(|(index, c)| {
         if c == to_remove {
             // Remvoe this char from the vec
-            let mut new_chars = chars.clone();
+            let mut new_chars = strings.clone();
 
             // Add the case where it is not remove
             if !perms.contains(&new_chars) {
@@ -71,8 +71,8 @@ fn permute_without(chars: Vec<char>, to_remove: &char) -> Vec<Vec<char>> {
 
 #[derive(Debug)]
 pub struct CFG {
-    starting_variable: char,
-    variables: HashMap<char, Variable>,
+    starting_variable: String,
+    variables: HashMap<String, Variable>,
 }
 
 impl CFG {
@@ -88,8 +88,12 @@ impl CFG {
         cfg
     }
 
-    fn get_variable(&self, name: char) -> &Variable {
-        self.variables.get(&name).unwrap()
+    fn get_variable(&self, name: &str) -> &Variable {
+        self.variables.get(name).unwrap()
+    }
+
+    fn is_variable(&self, name: &str) -> bool {
+        self.variables.contains_key(name)
     }
 
     // pub fn test(&self, string: &str) -> bool {}
@@ -107,14 +111,14 @@ impl CFG {
                 }
 
                 if production.len() > 2
-                    && production.iter().filter(|x| x.is_uppercase()).count() > 2
+                    && production.iter().filter(|x| self.is_variable(x)).count() > 2
                 {
                     return false;
                 }
 
                 // THIS ASSUMES THAT ALL VARIABLES ARE UPPERCASE
-                let all_uppercase = production.iter().all(|x| x.is_uppercase());
-                let all_lowercase = production.iter().all(|x| x.is_lowercase());
+                let all_uppercase = production.iter().all(|x| self.is_variable(x));
+                let all_lowercase = production.iter().all(|x| !self.is_variable(x));
 
                 if !(all_uppercase || all_lowercase) {
                     return false;
@@ -126,10 +130,6 @@ impl CFG {
     }
 
     fn to_cnf(&mut self) {
-        if self.is_cnf() {
-            return;
-        }
-
         // Step 1: Remove the start symbol from the RHS
         self.remove_start_symbol();
 
@@ -144,6 +144,7 @@ impl CFG {
         self.remove_useless_productions();
 
         // Step 3: Remove terminals from RHS if it exists with a variable
+
         // Step 4: Remove variables with more than 2 variables
     }
 
@@ -159,7 +160,7 @@ impl CFG {
         if contains_start {
             let old_starting_var = self.starting_variable.clone();
 
-            self.starting_variable = 'Z';
+            self.starting_variable = "Z".to_string();
             self.variables.insert(
                 self.starting_variable.clone(),
                 Variable {
@@ -175,7 +176,10 @@ impl CFG {
             .variables
             .iter()
             .filter_map(|(nullable_name, nullable_var)| {
-                let contains_null = nullable_var.productions.iter().any(|x| x.contains(&'#'));
+                let contains_null = nullable_var
+                    .productions
+                    .iter()
+                    .any(|x| x.contains(&"#".to_string()));
                 if contains_null {
                     Some(nullable_name.clone())
                 } else {
@@ -194,24 +198,24 @@ impl CFG {
                         .any(|x| x.contains(&nullable_name))
                 })
                 .for_each(|(_, containing_var)| {
-                    containing_var.remove_null_production(nullable_name.clone())
+                    containing_var.remove_null_production(nullable_name.clone().as_str())
                 });
         }
     }
 
     fn remove_unit_productions(&mut self) {
-        let mut unit_production_pairs: Vec<(char, char)> = vec![];
+        let mut unit_production_pairs: Vec<(String, String)> = vec![];
         self.variables.iter().for_each(|(name, var)| {
             var.productions.iter().for_each(|production| {
-                if production.len() == 1 && production[0].is_uppercase() {
-                    unit_production_pairs.push((name.clone(), production[0]));
+                if production.len() == 1 && self.is_variable(production[0].as_str()) {
+                    unit_production_pairs.push((name.clone(), production[0].clone()));
                 }
             });
         });
 
         // Add all the productions of the result to the parent and remove the unit production
         for (unit_name, unit_prod) in unit_production_pairs {
-            let result_prods = self.get_variable(unit_prod).productions.clone();
+            let result_prods = self.get_variable(unit_prod.as_str()).productions.clone();
             let parent_prod = &mut self.variables.get_mut(&unit_name).unwrap().productions;
             for prod in result_prods {
                 if !parent_prod.contains(&prod) {
@@ -225,16 +229,16 @@ impl CFG {
     }
 
     fn remove_useless_productions(&mut self) {
-        let mut reachable_vars: Vec<char> = vec![self.starting_variable];
+        let mut reachable_vars: Vec<String> = vec![self.starting_variable.clone()];
 
         loop {
             let mut new_reachable_vars = vec![];
 
             for var in &reachable_vars {
-                let var_prods = self.get_variable(*var).productions.clone();
+                let var_prods = self.get_variable(var.as_str()).productions.clone();
                 for prod in var_prods {
                     for symbol in prod {
-                        if symbol.is_uppercase()
+                        if self.is_variable(symbol.as_str())
                             && !reachable_vars.contains(&symbol)
                             && !new_reachable_vars.contains(&symbol)
                         {
@@ -250,7 +254,7 @@ impl CFG {
 
             new_reachable_vars.iter().for_each(|x| {
                 if !reachable_vars.contains(x) {
-                    reachable_vars.push(*x);
+                    reachable_vars.push(x.clone());
                 }
             });
         }
@@ -261,28 +265,28 @@ impl CFG {
     }
 }
 
-fn create_var_refs(lines: Vec<&str>) -> (char, HashMap<char, Variable>) {
-    let mut variables: HashMap<char, Variable> = HashMap::new();
-    let starting_variable = lines[0].chars().next().unwrap();
+fn create_var_refs(lines: Vec<&str>) -> (String, HashMap<String, Variable>) {
+    let mut variables: HashMap<String, Variable> = HashMap::new();
+    let starting_variable = lines[0].split(" ").next().unwrap().to_string();
 
     for line in lines {
         // First char is the variable name
-        let name = line.chars().next().unwrap();
-        let mut children = Vec::new();
+        let name = line.split(" ").next().unwrap().to_string();
+        let mut children: Vec<Vec<String>> = Vec::new();
 
-        let split = line.split("->");
+        let split = line.split(" -> ");
         let children_str = split.last().unwrap();
 
-        let children_sep = children_str.split("|").map(|x| x.trim());
+        let children_sep = children_str.split(" | ").map(|x| x.trim().split(" "));
 
         // Store just the name of the children for now
         for child in children_sep {
-            let child_vec: Vec<char> = child.chars().collect();
+            let child_vec: Vec<String> = child.map(|x| x.to_string()).collect();
             children.push(child_vec);
         }
 
         let variable = Variable {
-            name: name,
+            name: name.clone(),
             productions: children,
         };
 
@@ -293,7 +297,7 @@ fn create_var_refs(lines: Vec<&str>) -> (char, HashMap<char, Variable>) {
     (starting_variable, variables)
 }
 
-fn read_cfg() -> (char, HashMap<char, Variable>) {
+fn read_cfg() -> (String, HashMap<String, Variable>) {
     let file_data = std::fs::read_to_string("cfg.txt").unwrap();
     create_var_refs(file_data.lines().collect())
 }
@@ -301,13 +305,23 @@ fn read_cfg() -> (char, HashMap<char, Variable>) {
 #[cfg(test)]
 #[test]
 fn test_permute_without() {
-    let input = vec!['A', 'B', 'A', 'C'];
-    let output = permute_without(input, &'A');
+    let input = vec![
+        'A'.to_string(),
+        'B'.to_string(),
+        'A'.to_string(),
+        'C'.to_string(),
+    ];
+    let output = permute_without(input, "A");
     let expected = vec![
-        vec!['A', 'B', 'A', 'C'],
-        vec!['B', 'A', 'C'],
-        vec!['B', 'C'],
-        vec!['A', 'B', 'C'],
+        vec![
+            'A'.to_string(),
+            'B'.to_string(),
+            'A'.to_string(),
+            'C'.to_string(),
+        ],
+        vec!['B'.to_string(), 'A'.to_string(), 'C'.to_string()],
+        vec!['B'.to_string(), 'C'.to_string()],
+        vec!['A'.to_string(), 'B'.to_string(), 'C'.to_string()],
     ];
 
     assert_eq!(output, expected);
